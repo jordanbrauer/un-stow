5278fe0701e1b88b962b8afdbf9e670e912ccebd
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-07-06 18:04:09 -0500

    add helpers file for functional entry point to lib

---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Object oriented wraper for the PHP `pack` function, constructable via function.
+ *
+ * @param mixed ...$bytes
+ * @return Packable
+ */
+function stow(...$bytes): Packable {
+    return new \Stow(...$bytes);
+}
+
+/**
+ * Object oriented wraper for the PHP `unpack` function, constructable via function.
+ *
+ * @param mixed ...$bytes
+ * @return Unpackable
+ */
+function unstow(...$bytes): Unpackable {
+    return new \Unstow(...$bytes);
+}
