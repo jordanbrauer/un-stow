406dee58f858e5c960a8c5b7be985b570a5769c5
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-07-06 18:02:20 -0500

    add tests

---
+++ tests/StowTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+final class Stowtest extends TestCase
+{   
+    /**
+     * @dataProvider formats
+     * @param Closure $format
+     * @param mixed $expected
+     * @param mixed[] ...$bytes
+     * @return void
+     */
+    public function testPackingDataInVariousFormats(Closure $format, $expected, ...$bytes): void
+    {
+        $actual = $format(stow(...$bytes))->close();
+        
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Provides a closure that accepts a `Packable` and will return that
+     * Packable, unclosed. Expected and actual data are also supplied for the
+     * assertion method(s).
+     *
+     * @return Generator
+     */
+    public function formats(): Generator
+    {
+        yield from [
+            'Unsigned Short 16bit Big Endian (), Unsigned Short 16bit Little Endian (), Signed Char (*)' => [
+                function (Packable $packer): Packable {
+                    return $packer->unsignedShort('', Stow::BIG_ENDIAN_16_BIT)
+                        ->unsignedShort('', Stow::LITTLE_ENDIAN_16_BIT)
+                        ->signedChar('*');
+                },
+                "\x124xVAB",
+                0x1234, 0x5678, 65, 66,
+            ],
+            'Hex Nibble High (*)' => [
+                function (Packable $packer): Packable {
+                    return $packer->hexNibbleHigh('*');
+                },
+                'Hello World!',
+                '48656C6C6F20576F726C6421',
+            ],
+        ];
+    }
+}
---
+++ tests/UnstowTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+final class Unstowtest extends TestCase
+{   
+    /**
+     * @dataProvider formats
+     * @param Closure $format
+     * @param mixed $expected
+     * @param mixed[] ...$bytes
+     * @return void
+     */
+    public function testUnpackingDataInVariousFormats(Closure $format, $expected, ...$bytes): void
+    {
+        $actual = $format(unstow(...$bytes))->open();
+        
+        $this->assertInternalType('array', $actual);
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Provides a closure that accepts an `Unpackable` and will return that
+     * instance, unopened. Expected and actual data are also supplied for the
+     * assertion method(s).
+     *
+     * @return Generator
+     */
+    public function formats(): Generator
+    {
+        $stream = function (string $path, string $mode, int $read) {
+            $stream = fopen($path, $mode);
+            $bytes = fread($stream, $read);
+
+            fclose($stream);
+
+            return $bytes;
+        };
+        
+        yield from [
+            'GIF image header' => [
+                function (Unpackable $unpacker): Unpackable {
+                    return $unpacker->spacePaddedString(6, 'version')
+                        ->unsignedChar(2, 'width')
+                        ->unsignedChar(2, 'height')
+                        ->unsignedChar(1, 'flag')
+                        ->nullFill(11)
+                        ->unsignedChar(1, 'aspect');
+                }, [
+                    "version" => "GIF89a",
+                    "width1" => 135,
+                    "width2" => 0,
+                    "height1" => 180,
+                    "height2" => 0,
+                    "flag" => 212,
+                    "aspect" => 0,
+                ],
+                $stream('./tests/Portal.gif', 'rb', 20),
+            ]
+        ];
+    }
+}
